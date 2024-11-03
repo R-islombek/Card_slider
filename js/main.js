b809a9066a5de5d8d1e49ae569d8a71d55376bd4
R-islombek <islombekraximjanov97dev@gmail.com>
2024-11-03 22:38:35 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+      }
+  
+  });
+
+
+// 1-Masala
+
+// let R = prompt("R sonini kiriting");
+// let PI = 3;
+// L = 2 * PI * R;
+// S = PI * Math.pow(R, 2)
+
+
+// console.log(L, S);
+
+// 2-Masala;
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+
+// G = Math.sqrt(a * b);
+
+// console.log(G);
+
+// 3-Masala;
+
+// let x = prompt("x sonini kiriting");
+          
+// y = 3 * Math.pow(x,6) - 6 * Math.pow(x, 2) - 7;
+
+// console.log(y);
+
+// 4-Masala;
+
+// let kg = prompt("Kg ni kiriting");
+// let A = 1300;
+// let Kg = 2 ;
+// let Sum = 1300 /
+
+// let sum = 
+
+
